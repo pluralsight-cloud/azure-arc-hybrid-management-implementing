a1167df088f7ddf42e6d16f9c1f5ebbfb02610bd
Wayne Hoggett <wayne-hoggett@pluralsight.com>
2024-12-20 07:52:09 +1000

    Updates

--- LAB-Manage SQL Server with Azure Arc/Set-ArcSQLServer.ps1
+++ LAB-Manage SQL Server with Azure Arc/Set-ArcSQLServer.ps1
@@ -46,10 +46,9 @@
  New-Item -Path "C:\" -Name "Temp" -ItemType Directory -ErrorAction SilentlyContinue
  Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2215202&clcid=0x409&culture=en-us&country=us" -OutFile "C:\Temp\SQL2022-SSEI-Eval.exe"
  Set-Location -Path "C:\Temp"
-#  .\SQL2022-SSEI-Eval.exe /Quiet /HideProgressBar /Action=Download /MediaType=CAB /MediaPath=C:\SetupMedia /Language=en-US
-
+ 
  # Install SQL Server
-.\SQL2022-SSEI-Eval.exe /IAcceptSqlServerLicenseTerms /Quiet /Action=Install /MediaPath=C:\SetupMedia /Language=en-US
+.\SQL2022-SSEI-Eval.exe /IAcceptSqlServerLicenseTerms /Quiet /Action=Install /Language=en-US
  
  # Set a Scheduled Task to Disable the Azure VM Guest Agent and the Azure IMDS endpoint
  ## Ensure C:\Temp exists
