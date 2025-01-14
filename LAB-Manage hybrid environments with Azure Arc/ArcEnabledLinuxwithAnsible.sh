d57a7ba3a9efb0316b20fa8e43ace925abe561f3
Wayne Hoggett <wayne-hoggett@pluralsight.com>
2025-01-15 06:45:19 +1000

    Updates

--- LAB-Manage hybrid environments with Azure Arc/ArcEnabledLinuxwithAnsible.sh
+++ LAB-Manage hybrid environments with Azure Arc/ArcEnabledLinuxwithAnsible.sh
@@ -10,17 +10,3 @@ apt-get install -y ansible
 # Install sshpass (for Ansible connections using SSH passwords)
 apt-get update
 apt-get install -y sshpass
-
-# Install and enable the at command
-apt-get update 
-apt-get install -y at
-systemctl enable atd
-systemctl start atd
-
-# Configure for Arc-onboarding
-ufw --force enable
-ufw default allow incoming
-echo -e "sudo systemctl stop walinuxagent && sudo systemctl disable walinuxagent && sudo ufw deny out from any to 169.254.169.254" | at now + 3 minute
-
-# List scheduled at jobs
-atq
